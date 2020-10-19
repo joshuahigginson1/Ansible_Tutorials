de10b53dd2db9e964c50ec387ebb899713526072
Josh Higginson <joshuahigginson1@gmail.com>
2020-10-19 17:15:39 +0100

    Added Kitchen file & changed default apache2 index.html

---
+++ Chef/my_first_recipe/cookbooks/my_first_cookbook/recipes/default.rb
@@ -0,0 +1,14 @@
+#
+# Cookbook:: my_first_cookbook
+# Recipe:: default
+#
+# Copyright:: 2020, The Authors, All Rights Reserved.
+
+apt_package 'apache2' do
+  action :install
+end
+
+file '/var/www/html/index.html' do
+  content '<html>My Website!</html>'
+
+end
