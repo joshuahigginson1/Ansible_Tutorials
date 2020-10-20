8995d8557dd36e78c6e5954d28d482abae9597ef
Josh Higginson <joshuahigginson1@gmail.com>
2020-10-20 09:43:56 +0100

    Added Cross Compatibility with multiple linux platforms

--- Chef/my_first_recipe/cookbooks/my_first_cookbook/recipes/default.rb
+++ Chef/my_first_recipe/cookbooks/my_first_cookbook/recipes/default.rb
@@ -4,17 +4,22 @@
 #
 # Copyright:: 2020, The Authors, All Rights Reserved.
 
-# Cross Distro Variable
+# Cross Distro Variable.
 
 apache_package = value_for_platform_family(
   ['rhel', 'fedora', 'suse'] => 'httpd'
   'debian' => 'apache2'
 )
 
-# Install Apache
+# Install Apache.
 package apache_package
 
-# Add New File
+# Add New File.
 file '/var/www/html/index.html' do
   content '<html>My Website!</html>'
+end
+
+# Ensure that the apache service is started.
+service apache do
+  action [:enable, :start]
 end
