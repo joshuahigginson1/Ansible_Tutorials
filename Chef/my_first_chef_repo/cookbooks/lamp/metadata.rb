47adf40c881ba4dd3d9ba50bce3acb679b993c2a
Josh Higginson <joshuahigginson1@gmail.com>
2020-10-20 11:13:45 +0100

    Added kitchen config for EC2 AWS

---
+++ Chef/my_first_chef_repo/cookbooks/lamp/metadata.rb
@@ -0,0 +1,28 @@
+name 'lamp'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'All Rights Reserved'
+description 'Installs/Configures lamp'
+long_description 'Installs/Configures lamp'
+version '0.1.0'
+chef_version '>= 14.0'
+
+# The `issues_url` points to the location where issues for this cookbook are
+# tracked.  A `View Issues` link will be displayed on this cookbook's page when
+# uploaded to a Supermarket.
+#
+# issues_url 'https://github.com/<insert_org_here>/lamp/issues'
+
+# The `source_url` points to the development repository for this cookbook.  A
+# `View Source` link will be displayed on this cookbook's page when uploaded to
+# a Supermarket.
+#
+# source_url 'https://github.com/<insert_org_here>/lamp'
+
+# Berkshelf Dependencies
+
+depends 'mysql', '~> 8.5.1'
+depends 'mysql2_chef_gem', '~> 2.1.0'
+depends 'database', '~> 6.1.1'
+depends 'poise-python', '~> 1.7.0'
+depends 'apache2', '~> 3.2.2'
